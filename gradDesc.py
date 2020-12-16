52a5123f81d1e13e2ce5ae7bcdd0b524eb1b78ea
Thales Henrique <thales.henrique@engenharia.ufjf.br>
2020-12-16 15:00:16 -0300

    updates

--- gradDesc.py
+++ gradDesc.py
@@ -1,4 +1,55 @@
-#This a implementation of Gradient Descent algorith applied to minimize FOBs.
+# This a implementation of Gradient Descent algorith applied to minimize FOBs.
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# FOB
+def f_x(x):
+    return np.power(x + 2, 2) - 16 * np.exp(-np.power((x - 2), 2))
+
+
+def main_plot():
+    x = np.arange(-8, 8, 0.001) # Interval of x [-8,8]
+    y = map(lambda u: f_x(u), x)
+    plt.plot(x, list(y))
+
+#main_plot()
+#plt.show()
+
+
+def grad_f_x(x):
+    return (2 * x - 4) - 16 * (-2 * x + 4) * np.exp(-np.power(x - 2, 2))
+
+
+def gradient_descent(x0, func, grad): #x0 = ponto inicial
+    # precisão da solução
+    precision = 0.001
+    # Learning rate: muito pequeno pode demorar muito a convergir; muito grande pode fazer um 'overshoot' ao mínimo
+    learning_rate = 0.0001
+    # Temos de dar um limite ao número de iteracções
+    max_iter = 10000
+    x_new = x0
+    res = []
+    for i in range(max_iter):
+        x_old = x_new
+        # Vamos usar B = 1
+        x_new = x_old - learning_rate * grad(x_old)
+        f_x_new = func(x_new)
+        f_x_old = func(x_old)
+        res.append([x_new, f_x_new])
+        print(f_x_new - f_x_old)
+        if(abs (f_x_new - f_x_old) < precision):
+            print("Precisão %f alcançada:" % (f_x_new - f_x_old))
+            return np.array(res)
+    print("Iteracção máxima alcançada")
+    return np.array(res)
+
+# Vamos então tentar encontrar o mínimo da função começando com um valor inicial de x0 = -8
+
+
+x0 = 8
+res = gradient_descent(x0, f_x, grad_f_x)
+plt.plot(res[:, 0], res[:, 1], '+')
+main_plot()
+plt.show()
--- otimizationProb.py
+++ otimizationProb.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 #import matplotlib.cm as cm
 
 delta = 0.5
-x = np.arange(-10, 10, delta)
-y = np.arange(-10, 10, delta)
+x1 = np.arange(-10, 100, delta)
+x2 = np.arange(-100, 10, delta)
 
-X, Y = np.meshgrid(x, y)
-Z = X**2 + Y**2
+X1, X2 = np.meshgrid(x1, x2)
+Z = (X1 -1)**2 + 2*(X2**2)
 
 fig, ax = plt.subplots()
-CS = ax.contour(X, Y, Z, cmap=plt.get_cmap('rainbow'))
+CS = ax.contour(X1, X2, Z, cmap=plt.get_cmap('rainbow'))
 ax.clabel(CS, inline=1, fontsize=10)
 ax.set_title('f(x)')
 ax.set_xlabel('x1')
