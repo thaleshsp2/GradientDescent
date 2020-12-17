26d17f6c0b1db4f601e922e2c920807725190f2f
Thales Henrique <thales.henrique@engenharia.ufjf.br>
2020-12-17 15:33:48 -0300

    a few coments added

--- gradDesc.py
+++ gradDesc.py
@@ -7,27 +7,12 @@ def fob(x1, x2):
     grad = np.array([2*(x1-1), 4*x2])
     return fob, grad
 
-x1 = np.arange(-10, 10, 0.1)
-x2 = -x1
-x1, x2 = np.meshgrid(x1, x2)
-z = fob(x1, x2)
-fig, ax = plt.subplots(1)
-ax2 = ax.contour(x1, x2, z[0], cmap=plt.get_cmap('rainbow'))
-ax.clabel(ax2, inline=1, fontsize=10)
-plt.show()
-
-
-def fob(x1, x2):
-    fob = np.power(x1 - 1, 2) + 2 * np.power(x2, 2)
-    grad = np.array([2 * (x1 - 1), 4 * x2])
-    return fob, grad
-
 
 def dk(x0):
     return -fob(x0[0], x0[1])[1] / np.abs(fob(x0[0], x0[1])[0])
 
 
-def alpha(dk, x0):
+def alpha(dk, x0 ):
     alphax = (dk[0] - x0[0] * dk[0] - 2 * x0[1] * dk[1]) / (np.power(dk[0], 2) + 2 * np.power(dk[1], 2))
     return alphax
 
@@ -53,6 +38,18 @@ def gradientDescent(x0, fob):
     return np.array(res)
 
 
+# Fob plot
+x1 = np.arange(-10, 10, 0.1)
+x2 = -x1
+x1, x2 = np.meshgrid(x1, x2)
+z = fob(x1, x2)
+fig, ax = plt.subplots(1)
+ax2 = ax.contour(x1, x2, z[0], cmap=plt.get_cmap('rainbow'))
+ax.clabel(ax2, inline=1, fontsize=10)
+plt.show()
+
+
+# Iterations plot
 x0 = np.array([10, 10])
 res = gradientDescent(x0, fob)
 
@@ -62,7 +59,7 @@ plt.ylabel('FOB')
 plt.xlabel('Iterations')
 print(res)
 
-
+# Min plot
 fig, ax = plt.subplots(1)
 ax2 = ax.contour(x1, x2, z[0], cmap=plt.get_cmap('rainbow'))
 plt.plot(res[-1][1][0], res[-1][1][1], 'ro')
